f68813159d8d9e696616ee6e5d073086c43def0f
ngani faith <nganifaith@gmail.com>
2021-05-17 00:01:54 +0100

    Fix linters errors

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # library-app
+[](https://img.shields.io/badge/Microverse-blueviolet)
+
+
+
+
+## Built With
+
+- Javascript
+- Bootstrap
+- Html
+
+
+### Prerequisites
+Knowledge about JS:
+- Basic data structures
+- Loops
+- Arrays
+- Functions
+- Constructors
+- Rendering HTML in JS file
+- JS injection
+
+## Setup
+- git clone:  [Github repo](https://github.com/denisdiaconu/TicTacToe.git)
+- cd repo-folder
+- start program:  Ruby bin/main.rb on your command line or terminal
+
+## Author
+
+👤 **Ngani Faith**
+
+- GitHub: [@nganifaith](https://github.com/nganifaith)
+- Twitter: [@Bright_Ngani](https://twitter.com/bright_ngani)
+- LinkedIn: [Ngani Faith](https://www.linkedin.com/in/ngani-faith/)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/denisdiaconu/TicTacToe/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](https://choosealicense.com/licenses/mit/) licensed.
--- index.js
+++ index.js
@@ -1,57 +1,50 @@
 const books = [
-    new Book('Americana', 'Chimanda', 300, 'A young girl in a foriegn land', false),
-    new Book('Born Rich', 'Rick Aderson', 234, 'make money while young', true)
+  new Book(
+    "Americana",
+    "Chimanda",
+    300,
+    "A young girl in a foriegn land",
+    false
+  ),
+  new Book("Born Rich", "Rick Aderson", 234, "make money while young", true),
 ];
 
-const bookContainer = document.getElementById('books')
-const addButton = document.getElementById('add')
-const addBookForm = document.querySelector('form')
-
+const bookContainer = document.getElementById("books");
+const addButton = document.getElementById("add");
+const addBookForm = document.querySelector("form");
 
 function Book(title, author, pages, summary, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.summary = summary
-    this.read = read
-
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.summary = summary;
+  this.read = read;
 }
 
 function getBooks() {
-    bookContainer.innerHTML = ''
-    books.forEach((book, ind) => renderBook(book, ind))
+  bookContainer.innerHTML = "";
+  books.forEach((book, ind) => renderBook(book, ind));
 }
-
-
-
-
-
-
-
-
 
 function renderBook(book, ind) {
-    const template = `<div class="card mb-3 me-3" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${book.title}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
-                <p class="card-text">${book.summary}</p>
-                <p class="card-text">${book.pages}<span>Pages</span></p>
-                <button type="button" class="btn btn-info text-white card-link" id="toggle-${ind}" ${book.read? 'disabled' : ''}
-                onclick = "toggleRead">${book.read? 'Read' : 'Mark as read'}</button>
-                <button type="button" class="btn btn-danger text-white card-link" id="delete-${ind}">Delete</button>
-            </div>
-</div>`
-    bookContainer.innerHTML += template
-        // document.getElementById(`toggle-${ind}`).onclick = () => {
-        //     book.read = true
-        //     console.log('Hello')
-        //     getBooks()
-        // }
+  const template = `<div class="card mb-3 me-3" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
+            <p class="card-text">${book.summary}</p>
+            <p class="card-text">${book.pages}<span>Pages</span></p>
+            <button type="button" class="btn btn-info text-white card-link" 
+              id="toggle-${ind}" ${book.read ? "disabled" : ""}>${
+    book.read ? "Read" : "Mark as read"
+  }</button>
+            <button type="button" class="btn btn-danger text-white card-link" id="delete-${ind}">Delete</button>
+        </div>
+</div>`;
+  bookContainer.innerHTML += template;
 }
 
-getBooks()
+document.addEventListener("click", (event) => {
+  if (event.target) {}
+});
 
-function toggleRead(e) {
-    console.log(e)
-}
+getBooks();
